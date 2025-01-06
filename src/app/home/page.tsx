e7d6a4f11f7a28c86eb82f2b3a0f880b96fcac36
yangsunghun <163554102+yangsunghun@users.noreply.github.com>
2025-01-06 19:04:20 +0900

    chore: supabase 타입 동기화

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const MainPage = () => {
-  return <div>page</div>;
+  return (
+    <>
+      <section>
+        <div>page</div>
+      </section>
+      <section>
+        
+      </section>
+    </>
+  );
 };
 
 export default MainPage;

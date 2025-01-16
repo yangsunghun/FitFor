47bdcff2011c1acabd26bb9e858e3bd1c6645562
ovezen <klimen1005@gmail.com>
2025-01-16 10:36:29 +0900

    feat: 채팅방 메시지 유저 프로필, 닉네임 표시

--- src/lib/utils/chat/fetchMessages.ts
+++ src/lib/utils/chat/fetchMessages.ts
@@ -5,7 +5,17 @@ const supabase = createClient();
 export const fetchMessages = async (roomId: string) => {
   const { data, error } = await supabase
     .from("chat_messages")
-    .select("*")
+    .select(
+      `
+      *,
+      chat_members (
+        users (
+          nickname,
+          profile_image
+        )
+      )
+    `
+    )
     .eq("room_id", roomId)
     .order("created_at", { ascending: true });
 

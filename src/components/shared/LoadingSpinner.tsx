72250285303251581b573da59e606877efbd2eb5
chay140 <chaihyunpark140@gmail.com>
2025-01-19 01:09:25 +0900

    style: LoadingSpinner가 페이지 중심이 아닌 컴포넌트 중심에서 보이도록 변경

--- src/components/shared/LoadingSpinner.tsx
+++ src/components/shared/LoadingSpinner.tsx
@@ -2,7 +2,7 @@ import { CircleNotch } from "@phosphor-icons/react/dist/ssr";
 
 const LoadingSpinner = () => {
   return (
-    <div className="absolute inset-0 flex flex-col items-center justify-center">
+    <div className="flex h-full w-full flex-col items-center justify-center">
       <CircleNotch className="text-primary h-8 w-8 animate-spin" />
       <p className="text-muted-foreground mt-4 text-body">정보를 불러오는 중입니다.</p>
     </div>

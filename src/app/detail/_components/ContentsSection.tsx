e67957fe9090ab965e1c078b7978411ad79170ad
reizvoll <mara2277@naver.com>
2025-01-19 00:30:41 +0900

    feat: 게시글 줄바꿈 기능 추가 (ft. 엄정은)

--- src/app/detail/_components/ContentsSection.tsx
+++ src/app/detail/_components/ContentsSection.tsx
@@ -51,7 +51,8 @@ const ContentsSection = ({ postId, mode = "page" }: Props) => {
             <p className="text-text-03">{relativeTimeDay(created_at)}</p>
           </div>
 
-          <p className="mt-6 h-[8.5rem] overflow-auto text-title2 font-medium">{content}</p>
+          {/* 엄정은이 추가하라고 해서 추가한 줄바꿈.. 기능추가 ^-^)b */}
+          <p className="mt-6 h-[8.5rem] overflow-auto text-title2 font-medium whitespace-pre-wrap">{content}</p>
 
           {tags.length > 0 && (
             <div className="mt-10 flex flex-wrap gap-2">

48ea589fcdbfc3c2c81ed1f1ae9cda3ffd35ddbc
ovezen <klimen1005@gmail.com>
2025-01-19 15:18:04 +0900

    refactor: 채팅방 생성 폼 수정

--- src/app/chat/new/page.tsx
+++ src/app/chat/new/page.tsx
@@ -1,3 +1,26 @@
+import React from "react";
+import { z } from "zod";
+import type { chatRoomSchema } from "@/lib/validations/chatRoomSchema";
+import ChatRoomForm from "./_components/ChatRoomForm";
+
+export type ChatRoomFormInputs = z.infer<typeof chatRoomSchema>;
+
+const CreateRoomPage = () => {
+  return (
+    <div className="mx-auto max-w-[700px] pb-20 pt-10">
+      <div className="space-y-2 pb-10">
+        <h1 className="text-title1 font-bold leading-[150%] text-text-04">Live 만들기</h1>
+        <p className="text-title2 font-medium text-text-04">조언이 필요한 코디를 스타일 멘토와 공유해보세요!</p>
+      </div>
+      <div>
+        <ChatRoomForm />
+      </div>
+    </div>
+  );
+};
+
+export default CreateRoomPage;
+
 // 'use client'
 
 // import React, { useState } from "react";
@@ -120,119 +143,119 @@
 
 // export default ChatRoomCreationForm;
 
-"use client";
+// "use client";
 
-import React from "react";
-import { useForm, FormProvider } from "react-hook-form";
-import Summary from "./_components/Summary";
-import ThumbnailImage from "./_components/ThumbnailImage";
-import Tags from "./_components/TagInput";
-import { useFunnel } from "../_hooks/useFunnel";
-import ProgressBar from "./_components/ProgressBar";
-import { createRoomHandler } from "../_utils/createRoomHandler";
-import { useAuthStore } from "@/lib/store/authStore";
+// import React from "react";
+// import { useForm, FormProvider } from "react-hook-form";
+// import Summary from "./_components/Summary";
+// import ThumbnailImage from "./_components/ThumbnailImage";
+// import Tags from "./_components/TagInput";
+// import { useFunnel } from "../_hooks/useFunnel";
+// import ProgressBar from "./_components/ProgressBar";
+// import { createRoomHandler } from "../_utils/createRoomHandler";
+// import { useAuthStore } from "@/lib/store/authStore";
 
-const steps = ["Summary", "Thumbnail", "HashTags"];
+// const steps = ["Summary", "Thumbnail", "HashTags"];
 
-export interface FormDetails {
-  title: string;
-  subtitle: string;
-  description: string;
-  thumbnail: File;
-  hashtags: string[];
-  category: string;
-}
+// export interface FormDetails {
+//   title: string;
+//   subtitle: string;
+//   description: string;
+//   thumbnail: File;
+//   hashtags: string[];
+//   category: string;
+// }
 
-const Funnel = () => {
-  const currentUser = useAuthStore((state) => state.user);
-  const { Funnel, Step, next, prev, currentStep } = useFunnel(steps[0]);
-  const methods = useForm<FormDetails>({
-    defaultValues: {
-      title: "",
-      subtitle: "",
-      description: "",
-      thumbnail: undefined,
-      hashtags: [],
-      category: ""
-    }
-  });
+// const Funnel = () => {
+//   const currentUser = useAuthStore((state) => state.user);
+//   const { Funnel, Step, next, prev, currentStep } = useFunnel(steps[0]);
+//   const methods = useForm<FormDetails>({
+//     defaultValues: {
+//       title: "",
+//       subtitle: "",
+//       description: "",
+//       thumbnail: undefined,
+//       hashtags: [],
+//       category: ""
+//     }
+//   });
 
-  const currentStepIndex = steps.indexOf(currentStep);
+//   const currentStepIndex = steps.indexOf(currentStep);
 
-  const handleNextStep = methods.handleSubmit(() => {
-    next(steps[currentStepIndex + 1]); // 다음 단계로 이동
-  });
+//   const handleNextStep = methods.handleSubmit(() => {
+//     next(steps[currentStepIndex + 1]); // 다음 단계로 이동
+//   });
 
-  const handlePrevStep = () => {
-    prev(steps[currentStepIndex - 1]); // 이전 단계로 이동
-  };
+//   const handlePrevStep = () => {
+//     prev(steps[currentStepIndex - 1]); // 이전 단계로 이동
+//   };
 
-  const handleFormSubmit = methods.handleSubmit(async (data) => {
-    // 로그인 여부 확인
-    if (!currentUser || !currentUser.id) {
-      alert("로그인 상태를 확인해주세요.");
-      return;
-    }
+//   const handleFormSubmit = methods.handleSubmit(async (data) => {
+//     // 로그인 여부 확인
+//     if (!currentUser || !currentUser.id) {
+//       alert("로그인 상태를 확인해주세요.");
+//       return;
+//     }
 
-    // 썸네일 파일 확인
-    if (!data.thumbnail) {
-      alert("썸네일 파일을 선택해주세요.");
-      return;
-    }
+//     // 썸네일 파일 확인
+//     if (!data.thumbnail) {
+//       alert("썸네일 파일을 선택해주세요.");
+//       return;
+//     }
 
-    try {
-      // 채팅방 생성 요청
-      const { success, error } = await createRoomHandler(currentUser.id, data);
+//     try {
+//       // 채팅방 생성 요청
+//       const { success, error } = await createRoomHandler(currentUser.id, data);
 
-      if (!success) {
-        throw new Error(error);
-      }
+//       if (!success) {
+//         throw new Error(error);
+//       }
 
-      // 성공 처리
-      alert("채팅방이 성공적으로 생성되었습니다!");
-    } catch (error) {
-      // 에러 처리
-      console.error("채팅방 생성 실패:", error);
-    }
-  });
+//       // 성공 처리
+//       alert("채팅방이 성공적으로 생성되었습니다!");
+//     } catch (error) {
+//       // 에러 처리
+//       console.error("채팅방 생성 실패:", error);
+//     }
+//   });
 
-  return (
-    <FormProvider {...methods}>
-      <div className="flex h-screen flex-col">
-        <div className="flex-1 p-4">
-          <Funnel>
-            <Step name={steps[0]}>
-              <Summary />
-            </Step>
-            <Step name={steps[1]}>
-              <ThumbnailImage />
-            </Step>
-            <Step name={steps[2]}>
-              <Tags />
-            </Step>
-          </Funnel>
-        </div>
+//   return (
+//     <FormProvider {...methods}>
+//       <div className="flex h-screen flex-col">
+//         <div className="flex-1 p-4">
+//           <Funnel>
+//             <Step name={steps[0]}>
+//               <Summary />
+//             </Step>
+//             <Step name={steps[1]}>
+//               <ThumbnailImage />
+//             </Step>
+//             <Step name={steps[2]}>
+//               <Tags />
+//             </Step>
+//           </Funnel>
+//         </div>
 
-        <ProgressBar currentStepIndex={currentStepIndex} steps={steps} />
+//         <ProgressBar currentStepIndex={currentStepIndex} steps={steps} />
 
-        <div className="flex justify-between p-4">
-          <button
-            onClick={handlePrevStep}
-            className="rounded-lg bg-gray-200 px-6 py-2 text-black"
-            disabled={currentStepIndex === 0}
-          >
-            이전
-          </button>
-          <button
-            onClick={currentStepIndex === steps.length - 1 ? handleFormSubmit : handleNextStep}
-            className="rounded-lg bg-black px-6 py-2 text-white"
-          >
-            {currentStepIndex === steps.length - 1 ? "완료" : "다음"}
-          </button>
-        </div>
-      </div>
-    </FormProvider>
-  );
-};
+//         <div className="flex justify-between p-4">
+//           <button
+//             onClick={handlePrevStep}
+//             className="rounded-lg bg-gray-200 px-6 py-2 text-black"
+//             disabled={currentStepIndex === 0}
+//           >
+//             이전
+//           </button>
+//           <button
+//             onClick={currentStepIndex === steps.length - 1 ? handleFormSubmit : handleNextStep}
+//             className="rounded-lg bg-black px-6 py-2 text-white"
+//           >
+//             {currentStepIndex === steps.length - 1 ? "완료" : "다음"}
+//           </button>
+//         </div>
+//       </div>
+//     </FormProvider>
+//   );
+// };
 
-export default Funnel;
+// export default Funnel;

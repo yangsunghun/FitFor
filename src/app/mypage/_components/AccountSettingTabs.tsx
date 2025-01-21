adb1db39fd1b95af19fc1594b43c65c635f3147b
chay140 <chaihyunpark140@gmail.com>
2025-01-21 12:26:40 +0900

    feat: 개인 정보처리, 서비스 약관 업데이트

--- src/app/mypage/_components/AccountSettingTabs.tsx
+++ src/app/mypage/_components/AccountSettingTabs.tsx
@@ -3,30 +3,40 @@ import { useAuthStore } from "@/lib/store/authStore";
 import { CaretRight } from "@phosphor-icons/react";
 import { useState } from "react";
 import AccountDeleteContent from "./AccountDeleteContent";
+import AgreementContent from "./AgreementContent";
+import ServiceContent from "./ServiceContent";
 import SignoutButton from "./SignoutButton";
 
-const menuItems = [
+const menuItems: { title: string; mode: AccountModalMode }[] = [
   {
-    title: "약관 및 개인정보 처리 동의"
+    title: "서비스 약관",
+    mode: "serviceRule"
   },
   {
-    title: "개인정보 처리방침"
+    title: "개인정보 수집 처리방침",
+    mode: "agreement"
   },
   {
-    title: "모바일 버전"
-  }
+    title: "모바일 버전",
+    mode: "mobile"
+  },
+  { title: "탈퇴하기", mode: "deleteAccount" }
 ];
 
-type AccountModalMode = "deleteAccount" | "mobile" | "agreement" | "serviceRule" | ""
+type AccountModalMode = "agreement" | "serviceRule" | "deleteAccount" | "mobile" | "default";
 
 const AccountSettingTabs = () => {
   const { deleteUser } = useAuthStore();
   const [modal, setModal] = useState(false);
-  const [mode, setMode] = useState<AccountModalMode>("")
+  const [mode, setMode] = useState<AccountModalMode>("default");
 
   const openModal = (mode: AccountModalMode) => {
-    setModal(true);
-    setMode(mode)
+    if (mode !== "mobile") {
+      setModal(true);
+      setMode(mode);
+    } else {
+      handleClick();
+    }
   };
 
   const closeModal = () => {
@@ -46,7 +56,7 @@ const AccountSettingTabs = () => {
       <div className="mt-10 flex w-full flex-col space-y-6">
         {menuItems.map((item) => (
           <button
-            onClick={handleClick}
+            onClick={() => openModal(item.mode)}
             key={item.title}
             className="flex items-center justify-between px-6 py-4 text-title2 font-medium text-text-04 hover:bg-gray-50"
           >
@@ -54,18 +64,22 @@ const AccountSettingTabs = () => {
             <CaretRight size={24} />
           </button>
         ))}
-        <button
+        {/* <button
           onClick={() => openModal("deleteAccount")}
           className="flex items-center justify-between px-6 py-4 text-title2 font-medium text-text-04 hover:bg-gray-50"
         >
           <span>탈퇴하기</span>
           <CaretRight size={24} />
-        </button>
+        </button> */}
         <SignoutButton />
       </div>
 
       <ModalItem isOpen={modal} onClose={closeModal}>
-        <AccountDeleteContent closeModal={closeModal} handleDeleteAccount={handleDeleteAccount} />
+        {mode === "serviceRule" && <ServiceContent closeModal={closeModal} />}
+        {mode === "deleteAccount" && (
+          <AccountDeleteContent closeModal={closeModal} handleDeleteAccount={handleDeleteAccount} />
+        )}
+        {mode === "agreement" && <AgreementContent closeModal={closeModal} />}
       </ModalItem>
     </>
   );

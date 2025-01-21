a12cfa4292be00b0a1e1a03bfd4ac478b0991d76
chay140 <chaihyunpark140@gmail.com>
2025-01-22 00:23:33 +0900

    chore: 로그인 페이지 metadata 설정

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,4 +1,10 @@
+import type { Metadata } from "next";
 import LoginContent from "./_components/LoginContent";
+
+export const metadata: Metadata = {
+  title: "Fit4 - 로그인",
+  description: "Fit4 - 로그인 페이지입니다."
+};
 
 const LoginPage = () => {
   return (

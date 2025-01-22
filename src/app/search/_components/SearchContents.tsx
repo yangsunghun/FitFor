48425deec78e6c1ab52340fe49b9e764d2daf194
yangsunghun <163554102+yangsunghun@users.noreply.github.com>
2025-01-22 20:11:39 +0900

    style: 모바일 필터링 완벽하게 추가

--- src/app/search/_components/SearchContents.tsx
+++ src/app/search/_components/SearchContents.tsx
@@ -5,7 +5,9 @@ import SlideOver from "@/components/ui/SlideOver";
 import { useSearchPosts } from "@/lib/hooks/search/useSearchPosts";
 import { useSearchQuery } from "@/lib/hooks/search/useSearchQuery";
 import useCategoryStore from "@/lib/store/useCategoryStore";
-import { SlidersHorizontal } from "@phosphor-icons/react";
+import { CaretLeft, SlidersHorizontal, X } from "@phosphor-icons/react";
+import clsx from "clsx";
+import Link from "next/link";
 import { useEffect, useState } from "react";
 import Pagination from "./Pagination";
 import SearchResults from "./SearchResults";
@@ -16,9 +18,14 @@ import TagFilters from "./TagFilters";
 const SearchContents = () => {
   const { selectedCategory, isHydrated } = useCategoryStore();
   const [readyToRender, setReadyToRender] = useState(false);
-  const { query, page, tags, sort, handleSort } = useSearchQuery();
+  const { query, page, tags, sort, handleSort, handleToggleTag, resetTags } = useSearchQuery();
+
   const { Results, isPending, isError } = useSearchPosts(query, page, Object.values(tags).flat(), sort);
   const [isOpen, setIsOpen] = useState(false);
+
+  const selectedTags = Object.entries(tags).flatMap(([groupKey, groupTags]) =>
+    groupTags.map((tag) => ({ groupKey, tag }))
+  );
 
   useEffect(() => {
     if (isHydrated) {
@@ -34,14 +41,32 @@ const SearchContents = () => {
 
   return (
     <>
+      <Link href="/home">
+        <CaretLeft className="absolute left-0 top-[40px]" size={20} weight="bold" />
+      </Link>
       <section>
-        <TagFilters selectedGroup={selectedCategory} />
+        <TagFilters selectedGroup={selectedCategory} tags={tags} handleToggleTag={handleToggleTag} />
+        {selectedTags.length > 0 && (
+          <div className="absolute left-[35px] top-[82px] z-10 hidden flex-wrap gap-3 text-caption text-text-03 mb:flex">
+            {selectedTags.map(({ groupKey, tag }) => (
+              <div key={tag} className="flex items-center gap-1">
+                <span>{tag}</span>
+                <button onClick={() => handleToggleTag(groupKey, tag)}>
+                  <X />
+                </button>
+              </div>
+            ))}
+          </div>
+        )}
       </section>
 
       <div className="relative mb-10 flex items-center justify-end mb:mb-0 mb:h-[48px] mb:justify-between">
         <button
           onClick={() => setIsOpen(true)}
-          className="hidden h-[24px] w-[24px] items-center justify-center text-text-03 mb:flex"
+          className={clsx("hidden h-[24px] w-[24px] items-center justify-center mb:flex", {
+            "text-text-03": selectedTags.length === 0,
+            "text-primary-default": selectedTags.length > 0
+          })}
         >
           <SlidersHorizontal size={16} weight="fill" />
         </button>
@@ -55,7 +80,12 @@ const SearchContents = () => {
 
       {isOpen && (
         <SlideOver title="필터" onClose={() => setIsOpen(false)}>
-          <TagCheckFilters selectedGroup={selectedCategory} />
+          <TagCheckFilters
+            selectedGroup={selectedCategory}
+            tags={tags}
+            handleToggleTag={handleToggleTag}
+            resetTags={resetTags}
+          />
         </SlideOver>
       )}
     </>
--- src/app/search/_components/TagFilters.tsx
+++ src/app/search/_components/TagFilters.tsx
@@ -1,20 +1,19 @@
 "use client";
 import { Tags } from "@/components/ui/Tags";
 import { TAG_GROUPS } from "@/lib/constants/constants";
-import { useSearchQuery } from "@/lib/hooks/search/useSearchQuery";
 
 type TagFiltersProps = {
   selectedGroup: string | null;
+  tags: { [key: string]: string[] };
+  handleToggleTag: (groupKey: string, tag: string) => void;
 };
 
-const TagFilters = ({ selectedGroup }: TagFiltersProps) => {
-  const { tags, handleToggleTag } = useSearchQuery();
-
+const TagFilters = ({ selectedGroup, tags, handleToggleTag }: TagFiltersProps) => {
   return (
     <>
       {TAG_GROUPS.filter((group) => !selectedGroup || group.key === selectedGroup).map((group) => (
         <div key={group.key}>
-          <h2 className="mb-6 text-title1 font-bold mb:mb-0 mb:text-title2">{group.title}</h2>
+          <h2 className="mb-6 text-title1 font-bold mb:mb-0 mb:text-center mb:text-title2">{group.title}</h2>
 
           <div className="mb-6 flex flex-wrap gap-2 mb:hidden">
             {group.tags.map((tag) => (

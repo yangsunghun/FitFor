3cd0e15001d969259a0a2d6f2bdef62054138631
yangsunghun <163554102+yangsunghun@users.noreply.github.com>
2025-01-22 10:41:54 +0900

    refactor: confirm 을 modal 로 변경

--- src/app/detail/_components/EditDelete.tsx
+++ src/app/detail/_components/EditDelete.tsx
@@ -1,10 +1,13 @@
 "use client";
 
 import Dropdown from "@/components/ui/Dropdown";
+import ModalItem from "@/components/ui/Modal";
+import useModal from "@/lib/hooks/common/useModal";
 import { deletePost } from "@/lib/utils/post/deletePost";
 import { DotsThreeVertical } from "@phosphor-icons/react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
+import DeleteConfirm from "./DeleteConfirm";
 
 type Props = {
   postId: string;
@@ -12,32 +15,35 @@ type Props = {
 
 const EditDelete = ({ postId }: Props) => {
   const router = useRouter();
+  const { isOpen, openModal, closeModal } = useModal();
 
   const handleDeletePost = async () => {
-    const userConfirmed = window.confirm("정말 이 게시물을 삭제하시겠습니까?");
-    if (userConfirmed) {
-      const isDeleted = await deletePost(postId);
+    const isDeleted = await deletePost(postId);
 
-      if (isDeleted) {
-        alert("삭제되었습니다.");
-        router.replace("/");
-      } else {
-        alert("삭제 중 오류가 발생했습니다.");
-      }
+    if (isDeleted) {
+      alert("삭제되었습니다."); //alert 는 전부 토스트로 교체체
+      router.replace("/");
+    } else {
+      alert("삭제 중 오류가 발생했습니다.");
     }
   };
 
   return (
-    <Dropdown trigger={<DotsThreeVertical size={28} />} className="absolute right-0 top-3">
-      <ul>
-        <li className="w-full whitespace-nowrap py-2 text-left font-medium transition duration-300 hover:text-primary-default">
-          <Link href={`/write/${postId}`}>수정하기</Link>
-        </li>
-        <li className="w-full whitespace-nowrap py-2 text-left font-medium transition duration-300 hover:text-primary-default">
-          <button onClick={handleDeletePost}>삭제하기</button>
-        </li>
-      </ul>
-    </Dropdown>
+    <>
+      <Dropdown trigger={<DotsThreeVertical size={28} />} className="absolute right-0 top-3">
+        <ul>
+          <li className="w-full whitespace-nowrap py-2 text-left font-medium transition duration-300 hover:text-primary-default">
+            <Link href={`/write/${postId}`}>수정하기</Link>
+          </li>
+          <li className="w-full whitespace-nowrap py-2 text-left font-medium transition duration-300 hover:text-primary-default">
+            <button onClick={openModal}>삭제하기</button>
+          </li>
+        </ul>
+      </Dropdown>
+      <ModalItem isOpen={isOpen} onClose={closeModal}>
+        <DeleteConfirm closeModal={closeModal} handleDeletePost={handleDeletePost} />
+      </ModalItem>
+    </>
   );
 };
 

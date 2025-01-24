3c7b828c96fb1f151fc7bfc3e76915b5b0ba3971
yangsunghun <163554102+yangsunghun@users.noreply.github.com>
2025-01-24 17:29:16 +0900

    style: 최근 검색어, 자동완성 반응형

--- src/components/layout/SearchBar.tsx
+++ src/components/layout/SearchBar.tsx
@@ -62,14 +62,14 @@ const SearchBar = () => {
           className="h-12 w-full bg-transparent px-2 text-title2 font-medium outline-none placeholder:text-text-03 tb:h-[44px] tb:text-body tb:text-text-03"
         />
         {showDropdown && (filteredTags.length > 0 || searchHistory.length > 0) && (
-          <ul className="dropdown shadow-emphasize absolute left-0 top-[calc(100%+0.75rem)] z-50 w-full rounded-2xl bg-white p-6">
+          <ul className="dropdown shadow-emphasize absolute left-0 top-[calc(100%+0.75rem)] z-50 min-h-[360px] w-full rounded-2xl bg-white p-6 tb:top-[calc(100%+17px)] tb:rounded-none tb:p-0 tb:shadow-none">
             {/* 검색 기록 */}
             {searchHistory.length > 0 && inputValue.length === 0 && (
               <>
-                <li className="flex items-center justify-between py-2">
-                  <strong className="text-title2">최근 검색어</strong>
+                <li className="flex items-center justify-between py-2 tb:h-[55px] tb:p-0">
+                  <strong className="text-title2 tb:mt-[1px] tb:text-body tb:font-medium">최근 검색어</strong>
                   <button
-                    className="text-text-03"
+                    className="text-text-03 tb:text-caption"
                     onClick={(e) => {
                       e.preventDefault();
                       clearSearchHistory();
@@ -78,9 +78,12 @@ const SearchBar = () => {
                     전체 삭제
                   </button>
                 </li>
-                <li className="my-2 h-[1px] w-full bg-line-02"></li>
+                <li className="my-2 h-[1px] w-full bg-line-02 tb:my-0"></li>
                 {searchHistory.map((query, index) => (
-                  <li key={index} className="flex cursor-pointer justify-between py-2 text-title2 text-text-03">
+                  <li
+                    key={index}
+                    className="flex cursor-pointer justify-between py-2 text-title2 text-text-03 tb:h-[55px] tb:p-0 tb:text-body tb:font-medium"
+                  >
                     <button onClick={() => handleSelectHistory(query)} className="ellip1 flex-1 text-left">
                       {query}
                     </button>
@@ -91,7 +94,7 @@ const SearchBar = () => {
                         handleDeleteHistoryItem(index);
                       }}
                     >
-                      <X size={16} />
+                      <X size={16} weight="bold" />
                     </button>
                   </li>
                 ))}
@@ -100,12 +103,15 @@ const SearchBar = () => {
             {/* 자동완성(태그 중에서) */}
             {filteredTags.length > 0 && (
               <>
-                <li className="flex justify-between py-2">
-                  <strong className="text-title2">연관 검색어</strong>
+                <li className="flex items-center justify-between py-2 tb:h-[55px] tb:p-0">
+                  <strong className="text-title2 tb:mt-[1px] tb:text-body tb:font-medium">연관 검색어</strong>
                 </li>
-                <li className="my-2 h-[1px] w-full bg-line-02"></li>
+                <li className="my-2 h-[1px] w-full bg-line-02 tb:my-0"></li>
                 {filteredTags.map((tag) => (
-                  <li key={tag} className="cursor-pointer py-2 text-title2 text-text-03">
+                  <li
+                    key={tag}
+                    className="flex cursor-pointer justify-between py-2 text-title2 text-text-03 tb:h-[55px] tb:p-0 tb:text-body tb:font-medium"
+                  >
                     <button
                       onClick={() => {
                         handleSelectTag(tag);

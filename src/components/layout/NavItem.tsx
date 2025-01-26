488c29aa1e90c906e4d72617ba6e819633431d85
yangsunghun <163554102+yangsunghun@users.noreply.github.com>
2025-01-26 20:25:45 +0900

    fix: 경로에 따른 하단 바 아이콘 active 관련 수정

--- src/components/layout/NavItem.tsx
+++ src/components/layout/NavItem.tsx
@@ -6,23 +6,24 @@ import { ReactNode } from "react";
 
 type NavItemProps = {
   href: string;
+  location: string;
   icon: ReactNode;
   label: string;
 };
 
-const NavItem = ({ href, icon, label }: NavItemProps) => {
+const NavItem = ({ href, location, icon, label }: NavItemProps) => {
   const pathname = usePathname(); // 현재 경로
   const searchParams = useSearchParams(); // 현재 쿼리 파라미터
 
   const currentUrl = `${pathname}?${searchParams.toString()}`; // 경로 + 쿼리
-  const isActive = currentUrl.includes(href); // href가 현재 URL에 포함되어 있는지 확인
+  const isActive = currentUrl.includes(location); // href가 현재 URL에 포함되어 있는지 확인
   return (
     <li className="relative flex-1">
       <Link
         href={href}
         className={clsx(
           "flex flex-col items-center py-[4px] text-center transition-colors",
-          isActive ? "fill-primary-default font-bold text-primary-default" : "text-text-02"
+          isActive ? "font-bold text-primary-default" : "text-text-02"
         )}
       >
         {icon}

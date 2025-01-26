e0fd432731d8d30afe85515d16bef97d4acc8d6a
yangsunghun <163554102+yangsunghun@users.noreply.github.com>
2025-01-26 20:33:04 +0900

    refactor: 아이콘 html 컴포넌트화

--- src/components/layout/NavBar.tsx
+++ src/components/layout/NavBar.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Profile from "@/assets/images/Profile";
 import { useNavBarStore } from "@/lib/store/useNavBarStore";
 import { BookmarkSimple, House, MagnifyingGlass, VideoCamera } from "@phosphor-icons/react";
 import { Suspense } from "react";
@@ -22,21 +23,7 @@ const NavBar = () => {
             icon={<BookmarkSimple weight="fill" size={24} />}
             label="북마크"
           />
-          <NavItem
-            href="/mypage"
-            location="/mypage"
-            icon={
-              <svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                <path
-                  fill-rule="evenodd"
-                  clip-rule="evenodd"
-                  d="M4.50018 21C4.50018 21 3.00018 21 3.00018 19.5C3.00018 18 4.50018 13.5 12.0002 13.5C19.5002 13.5 21.0002 18 21.0002 19.5C21.0002 21 19.5002 21 19.5002 21H4.50018ZM12.0002 12C13.1937 12 14.3383 11.5259 15.1822 10.682C16.0261 9.83807 16.5002 8.69347 16.5002 7.5C16.5002 6.30653 16.0261 5.16193 15.1822 4.31802C14.3383 3.47411 13.1937 3 12.0002 3C10.8067 3 9.66212 3.47411 8.8182 4.31802C7.97429 5.16193 7.50018 6.30653 7.50018 7.5C7.50018 8.69347 7.97429 9.83807 8.8182 10.682C9.66212 11.5259 10.8067 12 12.0002 12V12Z"
-                  fill="currentColor"
-                />
-              </svg>
-            }
-            label="My"
-          />
+          <NavItem href="/mypage" location="/mypage" icon={<Profile />} label="My" />
         </Suspense>
       </ul>
     </nav>

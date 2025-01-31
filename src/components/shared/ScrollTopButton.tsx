1424479a47a0498c9134c928376b77e8a6d4a36f
reizvoll <mara2277@naver.com>
2025-01-31 16:34:16 +0900

    style: 시안 피드백에 따른 버튼 크기 수정 및 반응형 적용

--- src/components/shared/ScrollTopButton.tsx
+++ src/components/shared/ScrollTopButton.tsx
@@ -1,10 +1,14 @@
 "use client";
 
 import { ArrowUp } from "@phosphor-icons/react";
+import clsx from "clsx";
+import useMediaQuery from "@/lib/hooks/common/useMediaQuery";
 import { useEffect, useState } from "react";
 
 const ScrollTopButton = () => {
   const [isVisible, setIsVisible] = useState(false);
+  const isTabletOrSmaller = useMediaQuery("(max-width: 768px)"); // 반응형 여부 확인
+  const size = isTabletOrSmaller ? 24 : 48; // 디바이스 크기에 따라 아이콘 크기 설정
 
   // 스크롤 상태를 확인하여 버튼 표시 여부 결정
   useEffect(() => {
@@ -23,16 +27,22 @@ const ScrollTopButton = () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
   };
 
-  if (!isVisible) return null; // 버튼이 보이지 않을 때 렌더링 안 함
-
   return (
-    <button
-      onClick={scrollToTop}
-      className="fixed bottom-10 right-10 z-50 flex items-center justify-center w-12 h-12 rounded-full bg-primary-default text-bg-01 shadow-lg hover:bg-primary-dark focus:outline-none"
-      aria-label="최상단으로 이동"
-    >
-      <ArrowUp size={24} />
-    </button>
+    <div className="fixed bottom-12 right-[6.875rem] z-50 h-fit w-fit transition duration-300 tb:bottom-[80px] tb:right-[24px]">
+      <button
+        onClick={scrollToTop}
+        className={clsx(
+          "flex items-center justify-center rounded-full bg-primary-default text-bg-01 shadow-lg hover:bg-primary-dark focus:outline-none transition duration-300",
+          {
+            "h-[4.5rem] w-[4.5rem] tb:h-[40px] tb:w-[40px] opacity-100 pointer-events-auto": isVisible,
+            "opacity-0 pointer-events-none": !isVisible,
+          }
+        )}
+        aria-label="최상단으로 이동"
+      >
+        <ArrowUp size={size} weight="bold" />
+      </button>
+    </div>
   );
 };
 

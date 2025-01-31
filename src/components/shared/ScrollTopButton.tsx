6bf6ee0f21142f7a11ea0c61ce4ca5c672d6455a
reizvoll <mara2277@naver.com>
2025-02-01 01:31:38 +0900

    style: ScrollTopButton, 애니메이션 수정

--- src/components/shared/ScrollTopButton.tsx
+++ src/components/shared/ScrollTopButton.tsx
@@ -1,8 +1,8 @@
 "use client";
 
+import useMediaQuery from "@/lib/hooks/common/useMediaQuery";
 import { ArrowUp } from "@phosphor-icons/react";
 import clsx from "clsx";
-import useMediaQuery from "@/lib/hooks/common/useMediaQuery";
 import { useEffect, useState } from "react";
 
 type ScrollTopButtonProps = {
@@ -19,37 +19,33 @@ const ScrollTopButton = ({ useFlexLayout = false, extraBottomOffset = 0 }: Scrol
   useEffect(() => {
     const handleScroll = () => setIsVisible(window.scrollY > 200); // 200px 이상 스크롤 시 버튼 표시
     window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
+    return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
   // 최상단으로 이동
-  const scrollToTop = () => {
-    window.scrollTo({ top: 0, behavior: "smooth" });
-  };
+  const scrollToTop = () => window.scrollTo({ top: 0, behavior: "smooth" });
 
   return (
     <div
-      className={clsx(
-        "h-fit w-fit z-50 transition-all duration-300",
-        {
-          // 단독 사용 시 FloatingButton과 동일한 위치 적용
-          "fixed right-[6.875rem] tb:right-[24px] bottom-12 tb:bottom-[80px]": !useFlexLayout,
+      className={clsx("z-50 h-fit w-fit transition-all duration-300 ease-in-out", {
+        // 단독 사용 시 FloatingButton과 동일한 위치 적용
+        "fixed bottom-12 right-[6.875rem] tb:bottom-[80px] tb:right-[24px]": !useFlexLayout,
 
-          // 부모가 flex 레이아웃일 때는 relative 적용
-          "relative": useFlexLayout,
-        }
-      )}
+        // 부모가 flex 레이아웃일 때는 relative 적용
+        relative: useFlexLayout
+      })}
       style={useFlexLayout ? { bottom: `${12 + extraBottomOffset}px` } : {}}
     >
       <button
         onClick={scrollToTop}
         className={clsx(
-          "flex items-center justify-center rounded-full bg-primary-default text-bg-01 shadow-lg hover:bg-primary-dark focus:outline-none transition duration-300",
+          "hover:bg-primary-dark flex items-center justify-center rounded-full bg-primary-default text-bg-01 shadow-lg transition-all duration-300 ease-in-out focus:outline-none",
           {
-            "h-[4.5rem] w-[4.5rem] tb:h-[40px] tb:w-[40px] opacity-100 pointer-events-auto": isVisible,
-            "opacity-0 pointer-events-none": !isVisible,
+            // 나타날 때: 위로 올라오며 나타남
+            "pointer-events-auto h-[4.5rem] w-[4.5rem] translate-y-0 opacity-100 tb:h-[40px] tb:w-[40px]": isVisible,
+
+            // 사라질 때: 아래로 내려가며 사라짐
+            "pointer-events-none translate-y-4 opacity-0": !isVisible,
           }
         )}
         aria-label="최상단으로 이동"

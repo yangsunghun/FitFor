bb875861bff09c3288ed90d886ae5f51d99aaff5
chay140 <chaihyunpark140@gmail.com>
2025-01-31 14:58:16 +0900

    feat: 실수로 회원탈퇴 막기 위해 컨펌 input 추가

--- src/app/mypage/_components/AccountDeleteContent.tsx
+++ src/app/mypage/_components/AccountDeleteContent.tsx
@@ -1,29 +1,55 @@
 import { Button } from "@/components/ui/Button";
+import { TextField } from "@/components/ui/TextField";
 import useMediaQuery from "@/lib/hooks/common/useMediaQuery";
 import { useAuthStore } from "@/lib/store/authStore";
+import { useState } from "react";
 
 type AccountDeleteContentProps = {
   closeModal: () => void;
 };
 
 const AccountDeleteContent = ({ closeModal }: AccountDeleteContentProps) => {
-  const { deleteUser } = useAuthStore();
-
+  const { deleteUser, user } = useAuthStore();
+  const [inputValue, setInputValue] = useState("");
   const isTabletOrSmaller = useMediaQuery("(max-width: 768px)");
+
   const handleDeleteAccount = async () => {
-    deleteUser();
+    if (inputValue === user?.nickname) {
+      deleteUser();
+    }
   };
+
   return (
     <div className="flex w-[25.125rem] flex-col gap-4 tb:max-w-[19.375rem]">
       <p className="text-title1 font-bold text-text-04 tb:text-title2">정말 핏포를 탈퇴하실 건가요?</p>
       <p className="break-keep text-subtitle font-medium text-text-03 tb:text-body">
         계정 탈퇴 후 회원정보 파기로 커뮤니티 혹은 작성했던 게시물과 댓글은 삭제 처리가 불가해요.
       </p>
+
+      {/* 탈퇴 컨펌창 */}
+      <p className="text-body font-medium text-text-03">
+        탈퇴를 원하시면 아래 입력란에 <strong>{user?.nickname}</strong>을(를) 입력하세요.
+      </p>
+      <TextField
+        type="text"
+        value={inputValue}
+        variant={inputValue !== user?.nickname ? "error" : "default"}
+        onChange={(e) => setInputValue(e.target.value)}
+        className="w-full rounded-md border border-gray-300 p-2 text-text-04"
+        placeholder="사용자명을 입력하세요"
+      />
+
       <div className="flex w-full flex-row gap-3 tb:gap-2">
         <Button variant="whiteLine" size={isTabletOrSmaller ? "sm" : "lg"} className="w-full" onClick={closeModal}>
           취소하기
         </Button>
-        <Button size={isTabletOrSmaller ? "sm" : "lg"} className="w-full" onClick={handleDeleteAccount}>
+        <Button
+          size={isTabletOrSmaller ? "sm" : "lg"}
+          variant={inputValue !== user?.nickname ? "disabled" : "primary"}
+          className="w-full"
+          onClick={handleDeleteAccount}
+          disabled={inputValue !== user?.nickname} // Disable until username matches
+        >
           탈퇴하기
         </Button>
       </div>
--- src/app/mypage/_components/AccountSettingTabs.tsx
+++ src/app/mypage/_components/AccountSettingTabs.tsx
@@ -23,7 +23,7 @@ const AccountSettingTabs = () => {
     { title: "서비스 약관", mode: "serviceRule" },
     { title: "개인정보 수집 처리방침", mode: "agreement" },
     { title: "로그아웃", mode: "logout" },
-    { title: "탈퇴하기", mode: "deleteAccount" }
+    { title: "회원탈퇴", mode: "deleteAccount" }
   ];
 
   const openModal = (mode: AccountModalMode) => {
@@ -39,12 +39,12 @@ const AccountSettingTabs = () => {
 
   return (
     <>
-      <div className="mt-10 flex w-full flex-col gap-8 tb:gap-6 tb:mt-6 tb:px-4">
+      <div className="mt-10 flex w-full flex-col gap-8 tb:mt-6 tb:gap-6 tb:px-4">
         {menuItems.map((item) => (
           <button
             onClick={() => openModal(item.mode)}
             key={item.title}
-            className="flex items-center justify-between p-4 max-h-[3.25rem] text-title2 font-medium text-text-04 hover:bg-gray-50 tb:text-body tb:h-auto tb:p-0"
+            className="flex max-h-[3.25rem] items-center justify-between p-4 text-title2 font-medium text-text-04 hover:bg-gray-50 tb:h-auto tb:p-0 tb:text-body"
           >
             <span>{item.title}</span>
             <CaretRight className="text-title1 tb:text-body" />

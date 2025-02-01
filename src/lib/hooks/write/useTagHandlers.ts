5390aa650679d8b755ae86004ab7bb21609e7c53
reizvoll <mara2277@naver.com>
2025-02-02 02:18:35 +0900

    feat: 디자인 시안 변경에 따른 태그 섹션 재생성

--- src/lib/hooks/write/useTagHandlers.ts
+++ src/lib/hooks/write/useTagHandlers.ts
@@ -13,7 +13,7 @@ type UseTagHandlersProps = Pick<UseFormStateHandlersReturn, "formState"> & {
 
 export const useTagHandlers = ({ formState, handleFieldChange }: UseTagHandlersProps) => {
   const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
-
+  const Max = 4;
   // 카테고리 변경 핸들러
   const handleChangeCategory = (category: string) => {
     if (selectedCategory !== category) {
@@ -25,9 +25,9 @@ export const useTagHandlers = ({ formState, handleFieldChange }: UseTagHandlersP
   const toggleTagSelector = (tag: string, groupTags: string[], max: number) => {
     const selectedGroupTags = formState.tags.filter((t) => groupTags.includes(t));
 
-    // 전체 태그 수 제한 (최대 7개)
-    if (!selectedGroupTags.includes(tag) && formState.tags.length >= 7) {
-      toast("최대 7개의 태그만 선택 가능합니다.", "warning");
+    // 전체 태그 수 제한 (최대 4개)
+    if (!selectedGroupTags.includes(tag) && formState.tags.length >= Max) {
+      toast(`최대 ${Max}개의 태그만 선택 가능합니다.`, "warning");
       return;
     }
 

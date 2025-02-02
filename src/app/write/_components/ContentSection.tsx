420ff7279875d208629ca64563f3656a1b803bb6
reizvoll <mara2277@naver.com>
2025-02-03 00:12:11 +0900

    feat: 반응형 디자인 적용 (SubmitButton의 경우, 대공사 필요해서 우선 크기만 적용)

--- src/app/write/_components/ContentSection.tsx
+++ src/app/write/_components/ContentSection.tsx
@@ -1,4 +1,5 @@
 import { textFieldVariants } from "@/components/ui/TextField";
+import useMediaQuery from "@/lib/hooks/common/useMediaQuery";
 import { cn } from "@/lib/utils/common/className";
 
 type ContentSectionProps = {
@@ -7,26 +8,34 @@ type ContentSectionProps = {
   isMissing?: boolean; // 필수 입력 경고 표시 여부
 };
 
-const ContentSection = ({ content, onChange, isMissing }: ContentSectionProps) => (
+const ContentSection = ({ content, onChange, isMissing }: ContentSectionProps) => {
+  const isTabletOrSmaller = useMediaQuery("(max-width: 768px)");
+
+  return(
   <div className="space-y-2">
     {/* 제목 영역 */}
-    <div className="flex items-center gap-1">
-      <span className="text-title2 font-bold text-text-04">본문</span>
-      <span className="text-title2 font-bold text-primary-default">*</span>
+    <div className="flex items-center">
+      <span className="text-title2 font-bold text-text-04 mb:text-body mb:font-medium">본문</span>
+      <span className="text-title2 font-bold text-primary-default mb:text-body mb:font-medium">*</span>
     </div>
     {/* textarea 영역 */}
     <textarea
       value={content}
       onChange={(e) => onChange(e.target.value)} // 줄바꿈 포함된 텍스트 전달
-      rows={5}
+        rows={isTabletOrSmaller ? 3 : 5} // tb 이하에서는 rows 3 적용
       className={cn(
         textFieldVariants({ variant: "default", version: "desktop" }), // TextField 스타일 적용
-        "focus-visible:box-shadow-none h-auto w-full resize-none p-4 text-body font-medium placeholder-text-02 focus:outline-none focus-visible:border-transparent focus-visible:ring-0"
+        "focus-visible:box-shadow-none h-auto w-full resize-none p-4 text-body font-medium placeholder-text-02 focus:outline-none focus-visible:border-transparent focus-visible:ring-0 mb:px-3 mb:py:2"
       )}
-      placeholder="예시 - 소개팅 가야하는데 도와주세요"
+      placeholder={
+        isTabletOrSmaller
+          ? "어떤 룩인지 짧은 소개로 작성해보세요" // tb 이하에서의 placeholder
+          : "예시 - 소개팅 가야하는데 도와주세요"
+      }
     />
-    {isMissing && <p className="pt-2 text-body text-status-danger">본문을 입력해주세요.</p>}
+    {isMissing && <p className="pt-2 text-title2 font-medium text-status-danger tb:text-body">본문을 입력해주세요.</p>}
   </div>
-);
+  );
+};
 
 export default ContentSection;

5ff240cacbef254e82859f3b6a6ca2fa8d904916
yangsunghun <163554102+yangsunghun@users.noreply.github.com>
2025-02-02 21:47:28 +0900

    feat: 지역별 옷차림 페이지 추가

--- src/app/location/_components/LocationContents.tsx
+++ src/app/location/_components/LocationContents.tsx
@@ -1,21 +1,24 @@
 "use client";
+
 import Pagination from "@/app/search/_components/Pagination";
 import SearchResults from "@/app/search/_components/SearchResults";
 import SortPosts from "@/app/search/_components/SortPosts";
 import ErrorScreen from "@/components/common/ErrorScreen";
-import { useSearchPosts } from "@/lib/hooks/search/useSearchPosts";
-import { useSearchQuery } from "@/lib/hooks/search/useSearchQuery";
+import { useLocationQuery } from "@/lib/hooks/location/useLocationQuery";
+import { useLocationPosts } from "@/lib/hooks/location/useSearchPosts";
 import LocationTags from "./LocationTags";
 
 const LocationContents = () => {
-  const { query, page, tags, sort, handleSort, handleToggleTag, resetTags } = useSearchQuery();
-  const { Results, isPending, isError } = useSearchPosts(query, page, Object.values(tags).flat(), sort);
+  const { query, page, sort, handleSort } = useLocationQuery();
+  const { Results, isPending, isError } = useLocationPosts(query, page, sort);
 
   if (isError) return <ErrorScreen error={new Error("검색 데이터를 불러오는 중 에러가 발생했습니다.")} />;
 
   return (
     <>
-      <LocationTags />
+      <section className="mb-10">
+        <LocationTags />
+      </section>
       <div className="relative mb-10 flex items-center justify-between tb:mb-0 tb:h-[48px]">
         <p>
           총 <b>{Results?.total}</b>개
--- src/app/location/_components/LocationTags.tsx
+++ src/app/location/_components/LocationTags.tsx
@@ -2,34 +2,30 @@
 
 import { Tags } from "@/components/ui/Tags";
 import { REGIONS_WITH_QUERY } from "@/lib/constants/constants";
-import { memo, useState } from "react";
+import { useLocationQuery } from "@/lib/hooks/location/useLocationQuery";
+import { memo } from "react";
 
 type Props = {};
 
 const LocationTags = memo(({}: Props) => {
-  const [activeTag, setActiveTag] = useState<string | null>(null);
-
+  const { activeLocation, setActiveLocation } = useLocationQuery();
   const CURRENT_LOCATION_TAG = "현재 내 위치";
-
-  const handleTagClick = (tag: string) => {
-    setActiveTag(tag);
-  };
 
   return (
     <>
       <h2 className="mb-6 text-title1 font-bold mb:mb-0 mb:text-title2">지역별 옷차림</h2>
       <div className="flex flex-wrap gap-3">
-        <button className="focus:outline-none" onClick={() => handleTagClick(CURRENT_LOCATION_TAG)}>
+        <button className="focus:outline-none" onClick={() => setActiveLocation(CURRENT_LOCATION_TAG)}>
           <Tags
             size="lg"
             label={CURRENT_LOCATION_TAG}
-            variant={activeTag === CURRENT_LOCATION_TAG ? "black" : "gray"}
+            variant={activeLocation === CURRENT_LOCATION_TAG ? "black" : "gray"}
           />
         </button>
 
         {REGIONS_WITH_QUERY.map((tag) => (
-          <button key={tag.title} onClick={() => handleTagClick(tag.query)} className="focus:outline-none">
-            <Tags size="lg" label={tag.title} variant={activeTag === tag.query ? "black" : "gray"} />
+          <button key={tag.title} onClick={() => setActiveLocation(tag.query)} className="focus:outline-none">
+            <Tags size="lg" label={tag.title} variant={activeLocation === tag.query ? "black" : "gray"} />
           </button>
         ))}
       </div>

0f04dc77fd0ca584a77436389667fa7caa752b1a
yangsunghun <163554102+yangsunghun@users.noreply.github.com>
2025-02-02 23:00:46 +0900

    style: 에러 화면 반응형

--- src/components/common/ErrorScreen.tsx
+++ src/components/common/ErrorScreen.tsx
@@ -19,14 +19,14 @@ const ErrorScreen = ({ error, reset }: Props) => {
   return (
     <section className="fixed inset-0 m-auto flex items-center justify-center">
       <article className="text-center">
-        <h2 className="text-heading font-bold">지금 이 서비스와 연결할 수 없습니다!</h2>
-        <p className="mt-6 text-subtitle font-medium text-text-03">
+        <h2 className="text-heading font-bold mb:text-title1">지금 이 서비스와 연결할 수 없습니다!</h2>
+        <p className="mt-6 text-subtitle font-medium text-text-03 mb:text-body">
           문제를 해결하기 위해 열심히 노력하고 있습니다.
           <br />
           이용에 불편함을 드려 죄송합니다.
         </p>
 
-        <Image src={errorImage} alt="에러 이미지" className="my-16" />
+        <Image src={errorImage} alt="에러 이미지" className="my-16 tb:w-[80vw] mb:my-[40px]" />
 
         <div className="flex w-full justify-center gap-2">
           <Button asChild variant="primary" size="lg" className="min-w-40">

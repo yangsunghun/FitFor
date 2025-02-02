5da08a18d148d863a5c9fd4c4c066dd8c2a99faf
reizvoll <mara2277@naver.com>
2025-02-02 21:24:18 +0900

    feat: ImageSection, 반응형 작업 진행 (모바일 호버기능 수정 필요)

---
+++ src/app/write/_components/ImageUploadMobile.tsx
@@ -0,0 +1,133 @@
+"use client";
+
+import { Button } from "@/components/ui/Button";
+import { Check, Plus, Trash } from "@phosphor-icons/react";
+import Image from "next/image";
+import { Dispatch, SetStateAction } from "react";
+
+type ImageUploadProps = {
+  images: string[];
+  isMissing?: boolean;
+  handleFileInput: () => void;
+  handleDelete: (index: number) => void;
+  setImages: Dispatch<SetStateAction<string[]>>;
+  loadingStatus: boolean[];
+  blurDataCache: (string | null)[];
+};
+
+const MAX_IMAGES = 4; // 최대 이미지 개수!!!!!
+
+export const ImageUploadMobile = ({
+  images,
+  isMissing,
+  handleFileInput,
+  handleDelete,
+  setImages,
+  loadingStatus,
+  blurDataCache
+}: ImageUploadProps) => {
+  return (
+    <div className="space-y-4 pt-6">
+      {/* 제목 및 설명 */}
+      <div className="space-y-1">
+        <div className="flex items-center">
+          <span className="text-title2 font-medium text-text-04 mb:text-body">게시물 이미지</span>
+          <span className="text-lg font-medium text-primary-default">*</span>
+        </div>
+        <p className="text-body font-medium text-text-03 mb:text-caption">
+          다양한 각도에서 찍은 이미지가 있다면 추가해주세요.
+          <br />
+          추천 사이즈 : OOO x OOO / OOO x OOO
+        </p>
+      </div>
+
+      {/* 이미지 슬롯 섹션 */}
+      <div
+        className="overscroll-x-hidden no-scrollbar flex space-x-4 overflow-x-auto pb-4"
+        style={{
+          scrollbarWidth: "none", // Firefox용 인라인 스타일
+          msOverflowStyle: "none" // IE/Edge용 인라인 스타일
+        }}
+      >
+        {Array(MAX_IMAGES)
+          .fill(null)
+          .map((_, index) => {
+            const url = images[index];
+            const isUploading = loadingStatus[index];
+            const currentBlur = blurDataCache[index];
+
+            return (
+              <div
+                key={index}
+                className={`relative h-[8.65rem] w-[8.65rem] flex-shrink-0 items-center justify-center overflow-hidden rounded-lg mb:h-[7.5rem] mb:w-[7.5rem] ${
+                  url ? (index === 0 ? "border-2 border-primary-default" : "border border-line-02") : ""
+                }`}
+                onClick={!url ? handleFileInput : undefined}
+              >
+                {/* 빈 슬롯일 경우 */}
+                {!url && (
+                  <div className="absolute inset-0 flex items-center justify-center rounded-lg bg-neutral-100">
+                    <div className="flex h-[1.75rem] w-[1.75rem] items-center justify-center rounded-full bg-black-700 p-1.5">
+                      <Plus size={18} className="text-text-01" />
+                    </div>
+                  </div>
+                )}
+
+                {/* 블러 이미지 또는 업로드 중 메시지 */}
+                {currentBlur && (
+                  <Image src={currentBlur} alt="Uploading or Uploaded" layout="fill" className="object-cover" />
+                )}
+                {isUploading && (
+                  <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-40 text-white">
+                    <span>이미지 업로드 중...</span>
+                  </div>
+                )}
+
+                {/* 업로드된 이미지 */}
+                {url && (
+                  <>
+                    <Image src={url} alt={`Uploaded Image ${index + 1}`} layout="fill" className="object-cover" />
+                    <div className="absolute inset-0 flex flex-col items-center justify-center space-y-1 bg-black bg-opacity-50 opacity-0 transition-opacity hover:opacity-100">
+                      {/* 삭제 버튼 */}
+                      <button
+                        onClick={() => handleDelete(index)}
+                        className="absolute right-2 top-2 flex h-5 w-5 items-center justify-center rounded-full bg-bg-01 text-text-03"
+                      >
+                        <Trash size={12} />
+                      </button>
+                      {index !== 0 && (
+                        <Button
+                          variant="whiteLine"
+                          size="sm"
+                          className="text-sm h-5 w-20 border-none bg-transparent leading-none !text-text-01"
+                          onClick={() => {
+                            const updatedImages = [url, ...images.filter((img, i) => i !== index)];
+                            setImages(updatedImages);
+                          }}
+                        >
+                          썸네일 등록
+                        </Button>
+                      )}
+                    </div>
+                  </>
+                )}
+
+                {/* 첫 번째 이미지에 썸네일 표시 */}
+                {index === 0 && url && (
+                  <div className="text-xs absolute bottom-1 left-1 flex items-center gap-1 rounded bg-primary-default px-2 py-1 text-white">
+                    <Check size={8} weight="bold" />
+                    <span className="font-medium">썸네일</span>
+                  </div>
+                )}
+              </div>
+            );
+          })}
+      </div>
+
+      {/* 필수 입력 경고 메시지 */}
+      {isMissing && <p className="text-sm text-status-danger">이미지를 업로드해주세요.</p>}
+    </div>
+  );
+};
+
+export default ImageUploadMobile;

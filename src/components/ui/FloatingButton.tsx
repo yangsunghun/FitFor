45e57270ba0b8268a11429ef14cd0f44e829a1bb
ovezen <klimen1005@gmail.com>
2025-02-04 08:05:55 +0900

    chore: 플로팅 버튼 텍스트 변경(기본값: 새 글 작성하기)

--- src/components/ui/FloatingButton.tsx
+++ src/components/ui/FloatingButton.tsx
@@ -3,39 +3,26 @@ import useMediaQuery from "@/lib/hooks/common/useMediaQuery";
 import { Plus, X } from "@phosphor-icons/react";
 import clsx from "clsx";
 import Link from "next/link";
-import { useState, useEffect, type ReactNode } from "react";
+import { useState, type ReactNode } from "react";
 
 type Props = {
   href: string;
   icon: ReactNode;
-  useFlexLayout?: boolean;
-  onToggle?: (isOpen: boolean) => void; // 상태 전달 콜백
+  text?: string;
 };
 
-const FloatingButton = ({ href, icon, useFlexLayout = false, onToggle }: Props) => {
+const FloatingButton = ({ href, icon, text = "새 글 작성하기" }: Props) => {
   const [isOpen, setIsOpen] = useState(false);
-  const isTabletOrSmaller = useMediaQuery("(max-width: 768px)");
-  
-  useEffect(() => {
-    onToggle?.(isOpen);
-  }, [isOpen, onToggle]);
 
   const toggleOpen = () => {
     setIsOpen((prev) => !prev);
   };
 
+  const isTabletOrSmaller = useMediaQuery("(max-width: 768px)");
   const Icon = isOpen ? X : Plus; // isOpen 상태에 따라 사용할 컴포넌트 선택
   const size = isTabletOrSmaller ? 24 : 48; // 디바이스 크기에 따라 아이콘 크기 설정
-
   return (
-    <div
-      className={clsx(
-        !useFlexLayout
-          ? "fixed bottom-12 right-[6.875rem] tb:bottom-[80px] tb:right-[24px]"
-          : "relative",
-        "h-fit w-fit z-50 transition duration-300"
-      )}
-    >
+    <div className="fixed bottom-12 right-[6.875rem] z-50 h-fit w-fit transition duration-300 tb:bottom-[80px] tb:right-[24px]">
       <button
         onClick={toggleOpen}
         className={clsx(
@@ -55,7 +42,10 @@ const FloatingButton = ({ href, icon, useFlexLayout = false, onToggle }: Props) 
         )}
       >
         <li className="w-full whitespace-nowrap py-2 text-left font-medium tb:font-normal">
-          <Link href={href || "#"}>{icon}새 글 작성하기</Link>
+          <Link href={href || "#"}>
+            {icon}
+            {text}
+          </Link>
         </li>
       </ul>
     </div>

36ed9a5cc4af3887af894571469e6627e2dba2a0
yangsunghun <163554102+yangsunghun@users.noreply.github.com>
2025-02-06 18:17:29 +0900

    style: 디자인 QA

--- src/components/shared/GridPost.tsx
+++ src/components/shared/GridPost.tsx
@@ -13,6 +13,7 @@ type Props = {
 const GridPost = ({ post }: Props) => {
   const [isImgError, setIsImgError] = useState<boolean>(false);
   const isTabletOrSmaller = useMediaQuery("(max-width: 768px)");
+  const maxTags = isTabletOrSmaller ? 3 : 6;
 
   return (
     <li key={post.id} className="relative mb-4">
@@ -31,7 +32,7 @@ const GridPost = ({ post }: Props) => {
 
       <p className="ellip2 mt-4 text-title2 font-medium tb:mt-2 tb:text-body">{post.content}</p>
       <div className="mt-3 flex flex-wrap gap-2 tb:mt-2 tb:gap-1">
-        {post.tags.map((tag) => (
+        {post.tags.slice(0, maxTags).map((tag) => (
           <Tags key={tag} variant="grayLine" size="md" label={tag} />
         ))}
       </div>
